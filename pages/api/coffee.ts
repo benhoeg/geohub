e6b33d81393895ba66d4bc3a08f17c20b735074f
Ben <benhoeg@outlook.com>
2024-02-06 15:21:33 -0400

    get headers

--- pages/api/coffee.ts
+++ pages/api/coffee.ts
@@ -2,8 +2,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body)
-  console.log(req)
+  console.log(req.headers)
 
   res.status(200).send('All Good')
 }
